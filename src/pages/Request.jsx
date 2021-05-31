d0541f65df856e75571f8e676e3efb1f4a7c48f4
Jonathan García <jona03g97@gmail.com>
2021-05-30 22:13:22 -0600

    Agregar y enlistar request

--- src/pages/Request.jsx
+++ src/pages/Request.jsx
@@ -1,9 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { FaPen } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import Search from "../components/Search";
+import useGetData from "../hooks/useGetData";
+import "../styles/pages/Request.scss";
 
 const Request = () => {
+  const request = useGetData("https://portal-cesa.vercel.app/api/request");
+  const [searchValues, setSearchValues] = useState([]); //Resultados de busqueda
+  const [searchInputValue, setSearchInputValue] = useState(""); //Input de busqueda
+
+  useEffect(() => {
+    setSearchValues(request);
+  }, [request]);
+
+  const handleSearchChange = (event) => {
+    setSearchInputValue(event.target.value);
+  };
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    const newRequests = request.filter((requestItem) => {
+      if (
+        requestItem.client
+          .toLowerCase()
+          .includes(searchInputValue.toLowerCase())
+      ) {
+        return requestItem;
+      }
+    });
+
+    setSearchValues(newRequests);
+  };
   return (
     <>
-      <h1>Pedidos de servicio</h1>
+      <Search onChange={handleSearchChange} onSubmit={handleSearchSubmit} />
+      <section className="request">
+        <div className="request__table">
+          <h4>Client</h4>
+          <h4>Persona que llamo</h4>
+          <h4>Teléfono</h4>
+          <h4>Fecha y hora</h4>
+          <h4>Contestó</h4>
+          <h4>Detalle</h4>
+          <h4>Editar</h4>
+        </div>
+        {searchValues.map((requestItem) => {
+          return (
+            <div className="request__table" key={requestItem.id}>
+              <p>{requestItem.client}</p>
+              <p>{requestItem.name}</p>
+              <p>{requestItem.phone}</p>
+              <p>{requestItem.date}</p>
+              <p>{requestItem.attendant}</p>
+              <p>{requestItem.detail}</p>
+              <Link to={`/request/${requestItem.id}/edit`}>
+                <FaPen />
+              </Link>
+            </div>
+          );
+        })}
+      </section>
     </>
   );
 };
--- src/pages/RequestNew.jsx
+++ src/pages/RequestNew.jsx
@@ -1,10 +1,128 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import useSendData from "../hooks/useSendData";
+import "../styles/pages/New.scss";
 
 const RequestNew = () => {
+  const history = useHistory();
+  const [formValues, setFormValues] = useState({
+    client: "",
+    name: "",
+    phone: "",
+    date: "",
+    attendant: "",
+    detail: "",
+  });
+
+  const handleFormChange = (event) => {
+    setFormValues({
+      ...formValues,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    useSendData(
+      "https://portal-cesa.vercel.app/api/request",
+      "POST",
+      formValues
+    );
+    setTimeout(() => {
+      history.push("/request");
+    }, 1500);
+  };
   return (
-    <>
-      <h1>Pedido de servicio nuevo</h1>
-    </>
+    <section className="add">
+      <h3>AGREGAR UN NUEVO PEDIDO DE SERVICIO</h3>
+      <form
+        action=""
+        id="add__form"
+        className="add__form"
+        onSubmit={handleFormSubmit}
+      >
+        <div className="add_form-element-container">
+          <label htmlFor="client">
+            <p>CLIENTE</p>
+            <input
+              onChange={handleFormChange}
+              type="text"
+              name="client"
+              id="client"
+              placeholder="Cliente"
+              className="add__form-input"
+              value={formValues.client}
+            />
+          </label>
+          <label htmlFor="name">
+            <p>PERSONA QUE LLAMÓ</p>
+            <input
+              onChange={handleFormChange}
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Nombre de quien llamó"
+              required
+              className="add__form-input"
+              value={formValues.name}
+            />
+          </label>
+          <label htmlFor="phone">
+            <p>TELÉFONO</p>
+            <input
+              onChange={handleFormChange}
+              type="number"
+              name="phone"
+              id="phone"
+              placeholder="2222-2222"
+              required
+              className="add__form-input"
+              value={formValues.phone}
+            />
+          </label>
+          <label htmlFor="date">
+            <p>FECHA</p>
+            <input
+              onChange={handleFormChange}
+              type="datetime-local"
+              name="date"
+              id="date"
+              className="add__form-input"
+              value={formValues.date}
+            />
+          </label>
+          <label htmlFor="attendant">
+            <p>PERSONA QUE ATENDIÓ</p>
+            <input
+              onChange={handleFormChange}
+              type="text"
+              name="attendant"
+              id="attendant"
+              placeholder="Persona de monitoreo"
+              required
+              className="add__form-input"
+              value={formValues.attendant}
+            />
+          </label>
+          <label htmlFor="detail">
+            <p>DETALLES</p>
+            <input
+              onChange={handleFormChange}
+              type="text"
+              name="detail"
+              id="detail"
+              placeholder="Detalles"
+              required
+              className="add__form-input"
+              value={formValues.detail}
+            />
+          </label>
+        </div>
+        <button type="submit" id="add__button">
+          Agregar Contacto
+        </button>
+      </form>
+    </section>
   );
 };
 
