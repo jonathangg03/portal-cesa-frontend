0692d9a18588d81a00935346b5d8c9bf8821d757
Jonathan García <jona03g97@gmail.com>
2021-07-21 20:56:45 -0600

    Add send data on ContactNew

--- src/pages/ContactNew.jsx
+++ src/pages/ContactNew.jsx
@@ -1,181 +1,201 @@
-import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
-import useSendData from "../hooks/useSendData";
+import React, { Component } from "react";
+import sendData from "../utils/sendData";
+import Loading from "../components/Loading";
 import config from "../config";
 import "../styles/pages/New.scss";
 
-const ContactNew = () => {
-  const history = useHistory();
-  const [formValues, setFormValues] = useState({
-    firstName: "",
-    secondName: "",
-    firstLastname: "",
-    secondLastname: "",
-    email: "",
-    prefix: "",
-    phone: "",
-    cellphone: "",
-    extension: "",
-    tag: "",
-  });
+class ContactNew extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formValues: {
+        firstName: "",
+        secondName: "",
+        firstLastname: "",
+        secondLastname: "",
+        email: "",
+        prefix: "",
+        phone: "",
+        cellphone: "",
+        extension: "",
+        tag: "",
+      },
+      loading: false,
+      error: null,
+    };
+  }
 
-  const handleFormChange = (event) => {
-    setFormValues({
-      ...formValues,
-      [event.target.name]: event.target.value,
+  handleFormChange = (event) => {
+    this.setState({
+      ...this.state,
+      formValues: {
+        ...this.state.formValues,
+        [event.target.name]: event.target.value,
+      },
     });
   };
 
-  const handleFormSubmit = (event) => {
+  handleFormSubmit = async (event) => {
     event.preventDefault();
-    useSendData(`${config.api}/api/contact`, "POST", formValues);
-    setTimeout(() => {
-      history.push("/contact");
-    }, 1000);
+    this.setState({ ...this.state, loading: true });
+    try {
+      await sendData(
+        `${config.api}/api/contact`,
+        "POST",
+        this.state.formValues
+      );
+      this.setState({ ...this.state, loading: false });
+      this.props.history.push("/contact");
+    } catch (error) {
+      this.setState({ ...this.state, loading: false, error: error });
+    }
   };
 
-  return (
-    <section className="add">
-      <h3>AGREGAR UN NUEVO CONTACTO</h3>
-      <form
-        action=""
-        id="add__form"
-        className="add__form"
-        onSubmit={handleFormSubmit}
-      >
-        <div className="add_form-element-container">
-          <label htmlFor="firstName">
-            <p>PRIMER NOMBRE</p>
-            <input
-              onChange={handleFormChange}
-              type="text"
-              name="firstName"
-              id="firstName"
-              placeholder="Primer nombre"
-              required
-              className="add__form-input"
-              value={formValues.firstName}
-            />
-          </label>
-          <label htmlFor="secondName">
-            <p>SEGUNDO NOMBRE</p>
-            <input
-              onChange={handleFormChange}
-              type="text"
-              name="secondName"
-              id="secondName"
-              placeholder="Segundo nombre"
-              className="add__form-input"
-              value={formValues.secondName}
-            />
-          </label>
-          <label htmlFor="firstLastname">
-            <p>APELLIDO PATERNO</p>
-            <input
-              onChange={handleFormChange}
-              type="text"
-              name="firstLastname"
-              id="firstLastname"
-              placeholder="Apellido paterno"
-              required
-              className="add__form-input"
-              value={formValues.firstLastname}
-            />
-          </label>
-          <label htmlFor="secondLastname">
-            <p>APELLIDO MATERNO</p>
-            <input
-              onChange={handleFormChange}
-              type="text"
-              name="secondLastname"
-              id="secondLastname"
-              placeholder="Apellido materno"
-              className="add__form-input"
-              value={formValues.secondLastname}
-            />
-          </label>
-          <label htmlFor="email">
-            <p>CORREO</p>
-            <input
-              onChange={handleFormChange}
-              type="text"
-              name="email"
-              id="email"
-              placeholder="napellido@grupocesa.com"
-              required
-              className="add__form-input"
-              value={formValues.email}
-            />
-          </label>
-          <label htmlFor="prefix">
-            <p>PREFIJO</p>
-            <input
-              onChange={handleFormChange}
-              type="text"
-              name="prefix"
-              id="prefix"
-              placeholder="+506"
-              required
-              className="add__form-input"
-              value={formValues.prefix}
-            />
-          </label>
-          <label htmlFor="phone">
-            <p>TELÉFONO</p>
-            <input
-              onChange={handleFormChange}
-              type="number"
-              name="phone"
-              id="phone"
-              placeholder="2202-2600"
-              required
-              className="add__form-input"
-              value={formValues.phone}
-            />
-          </label>
-          <label htmlFor="cellphone">
-            <p>CELULAR</p>
-            <input
-              onChange={handleFormChange}
-              type="number"
-              name="cellphone"
-              id="cellphone"
-              placeholder="2202-2600"
-              className="add__form-input"
-              value={formValues.cellphone}
-            />
-          </label>
-          <label htmlFor="extension">
-            <p>EXTENSIÓN</p>
-            <input
-              onChange={handleFormChange}
-              type="number"
-              name="extension"
-              id="extension"
-              placeholder="2600"
-              className="add__form-input"
-              value={formValues.extension}
-            />
-          </label>
-          <label htmlFor="tags">
-            <p>TAGS</p>
-            <input
-              onChange={handleFormChange}
-              type="text"
-              name="tag"
-              id="tags"
-              placeholder="bd, redes, apodo"
-              className="add__form-input"
-              value={formValues.tag}
-            />
-          </label>
-        </div>
-        <button type="submit" id="add__button">
-          Agregar Contacto
-        </button>
-      </form>
-    </section>
-  );
-};
+  render() {
+    return (
+      <section className="add">
+        <h3>AGREGAR UN NUEVO CONTACTO</h3>
+        <form
+          action=""
+          id="add__form"
+          className="add__form"
+          onSubmit={this.handleFormSubmit}
+        >
+          <div className="add_form-element-container">
+            <label htmlFor="firstName">
+              <p>PRIMER NOMBRE</p>
+              <input
+                onChange={this.handleFormChange}
+                type="text"
+                name="firstName"
+                id="firstName"
+                placeholder="Primer nombre"
+                required
+                className="add__form-input"
+                value={this.state.formValues.firstName}
+              />
+            </label>
+            <label htmlFor="secondName">
+              <p>SEGUNDO NOMBRE</p>
+              <input
+                onChange={this.handleFormChange}
+                type="text"
+                name="secondName"
+                id="secondName"
+                placeholder="Segundo nombre"
+                className="add__form-input"
+                value={this.state.formValues.secondName}
+              />
+            </label>
+            <label htmlFor="firstLastname">
+              <p>APELLIDO PATERNO</p>
+              <input
+                onChange={this.handleFormChange}
+                type="text"
+                name="firstLastname"
+                id="firstLastname"
+                placeholder="Apellido paterno"
+                required
+                className="add__form-input"
+                value={this.state.formValues.firstLastname}
+              />
+            </label>
+            <label htmlFor="secondLastname">
+              <p>APELLIDO MATERNO</p>
+              <input
+                onChange={this.handleFormChange}
+                type="text"
+                name="secondLastname"
+                id="secondLastname"
+                placeholder="Apellido materno"
+                className="add__form-input"
+                value={this.state.formValues.secondLastname}
+              />
+            </label>
+            <label htmlFor="email">
+              <p>CORREO</p>
+              <input
+                onChange={this.handleFormChange}
+                type="text"
+                name="email"
+                id="email"
+                placeholder="napellido@grupocesa.com"
+                required
+                className="add__form-input"
+                value={this.state.formValues.email}
+              />
+            </label>
+            <label htmlFor="prefix">
+              <p>PREFIJO</p>
+              <input
+                onChange={this.handleFormChange}
+                type="text"
+                name="prefix"
+                id="prefix"
+                placeholder="+506"
+                required
+                className="add__form-input"
+                value={this.state.formValues.prefix}
+              />
+            </label>
+            <label htmlFor="phone">
+              <p>TELÉFONO</p>
+              <input
+                onChange={this.handleFormChange}
+                type="number"
+                name="phone"
+                id="phone"
+                placeholder="2202-2600"
+                required
+                className="add__form-input"
+                value={this.state.formValues.phone}
+              />
+            </label>
+            <label htmlFor="cellphone">
+              <p>CELULAR</p>
+              <input
+                onChange={this.handleFormChange}
+                type="number"
+                name="cellphone"
+                id="cellphone"
+                placeholder="2202-2600"
+                className="add__form-input"
+                value={this.state.formValues.cellphone}
+              />
+            </label>
+            <label htmlFor="extension">
+              <p>EXTENSIÓN</p>
+              <input
+                onChange={this.handleFormChange}
+                type="number"
+                name="extension"
+                id="extension"
+                placeholder="2600"
+                className="add__form-input"
+                value={this.state.formValues.extension}
+              />
+            </label>
+            <label htmlFor="tags">
+              <p>TAGS</p>
+              <input
+                onChange={this.handleFormChange}
+                type="text"
+                name="tag"
+                id="tags"
+                placeholder="bd, redes, apodo"
+                className="add__form-input"
+                value={this.state.formValues.tag}
+              />
+            </label>
+          </div>
+          <button type="submit" id="add__button">
+            Agregar Contacto
+          </button>
+        </form>
+        {this.state.loading && <Loading />}
+      </section>
+    );
+  }
+}
 
 export default ContactNew;
